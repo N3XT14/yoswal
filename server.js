a0519411120a56ba23993547f57de701a3720a5f
N3XT14 <yashoswal1405@gmail.com>
2022-04-20 17:40:21 +0530

    Skill Section Template Added

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // Imports
 const express = require("express");
 const cors = require("cors");
-const path = require('path');
 require("dotenv").config({ path: "./config.env" });
+const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
