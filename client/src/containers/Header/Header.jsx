544e34ed9f07499628edb22cf0e1b9e329f0eda2
N3XT14 <yashoswal1405@gmail.com>
2022-06-25 19:51:34 +0530

    Change in Header Description

--- client/src/containers/Header/Header.jsx
+++ client/src/containers/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Fragment } from "react";
 
 import images from "../../constants/images.js";
 import Wave from "./Wave/Wave.jsx";
 import "./Header.scss";
 
 const Header = () => {
+  const space = <Fragment>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Fragment>
+
   return (
     <section id="home" className="home">
       <div className="app__header app__flex">
@@ -15,13 +18,17 @@ const Header = () => {
           <span>
             I'm
             <strong className="p-text main-name"> Yash Oswal</strong>
-          </span>
-          <p className="p-text">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea, enim!
-            Beatae, quisquam. At ex, deserunt architecto iste dignissimos
-            nesciunt laboriosam accusantium nostrum quod culpa quos dolor unde
-            facere explicabo dolorum!
-          </p>
+          </span>          
+          <code className="main-name">            
+            <d style={{ fontSize: 20, color: 'orange' }}>while()</d> noSuccess:<br />
+            {space}<d style={{ fontSize: 20, color: 'blue' }}>code()</d><br />
+            {space}<d style={{ fontSize: 20, color: 'blue' }}>drinkCoffee()</d><br />
+            {space}<d style={{ fontSize: 20, color: 'blue' }}>code()</d><br />
+            {space}<d style={{ fontSize: 20, color: 'blue' }}>drinkCoffee()</d><br />
+            {space}<d style={{ fontSize: 20, color: 'orange' }}>if</d> dead:<br />            
+            {space}{space}break<br />
+          </code>
+          <br />
         </div>
         <div className="app__header-img">
           <img src={images.profile} alt="" />
