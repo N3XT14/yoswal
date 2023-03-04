103d45d959da6e9e9abae1d4b8d1c370b3c3c4ca
N3XT <44130176+N3XT14@users.noreply.github.com>
2023-03-04 13:21:08 -0500

    Serverless Connection

--- db/conn.js
+++ db/conn.js
@@ -8,14 +8,18 @@ const client = new MongoClient(Db, {
 var _db;
 
 module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      if (db) {
-        _db = db.db(process.env.DB_NAME);
-        console.log("Successfully connected to MongoDB.");
-      }
+  connectToServer: async function (callback) {
+    console.log("Reached connectToServer");
+    try {
+      const db = await client.connect();
+      console.log("Inside connectToServer");
+      _db = db.db(process.env.DB_NAME);
+      console.log("Successfully connected to MongoDB.");
+      return callback(null);
+    } catch (err) {
       return callback(err);
-    });
+    }
+    console.log("Reached connectToServer End");
   },
 
   getDb: function () {
